3f2f68eadb7cd43c712e88ffb46412ffa160a189
Eugene <87051223+Swordaac@users.noreply.github.com>
2024-02-22 14:00:12 -0500

    Add files via upload

---
+++ shellmemory.c
@@ -0,0 +1,269 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include <time.h>
+
+#define PAGE_LINES 3
+
+
+
+
+#define SHELL_MEM_LENGTH 1000
+
+const int FRAME_STORE_SIZE = 5;
+const int FRAME_SIZE = 3;
+const int THRESHOLD = FRAME_STORE_SIZE * FRAME_SIZE -1;
+
+struct memory_struct{
+	char *var;
+	char *value;
+};
+
+struct memory_struct shellmemory[SHELL_MEM_LENGTH];
+
+void resetmem() {
+    // Start clearing from THRESHOLD + 1 to respect the frame store
+    for (int i = THRESHOLD + 1; i < SHELL_MEM_LENGTH; i++) {
+        if (shellmemory[i].var != NULL) {
+            free(shellmemory[i].var); // Free existing string to prevent memory leaks
+        }
+        if (shellmemory[i].value != NULL) {
+            free(shellmemory[i].value); // Free existing string to prevent memory leaks
+        }
+        shellmemory[i].var = strdup("none");
+        shellmemory[i].value = strdup("none");
+    }
+}
+
+//helper function to alloc a new frame
+
+// Helper functions
+int match(char *model, char *var) {
+	int i, len=strlen(var), matchCount=0;
+	for(i=0;i<len;i++)
+		if (*(model+i) == *(var+i)) matchCount++;
+	if (matchCount == len)
+		return 1;
+	else
+		return 0;
+}
+
+char *extract(char *model) {
+	char token='=';    // look for this to find value
+	char value[1000];  // stores the extract value
+	int i,j, len=strlen(model);
+	for(i=0;i<len && *(model+i)!=token;i++); // loop till we get there
+	// extract the value
+	for(i=i+1,j=0;i<len;i++,j++) value[j]=*(model+i);
+	value[j]='\0';
+	return strdup(value);
+}
+
+
+// Shell memory functions
+
+void mem_init(){
+	int i;
+	for (i=0; i<1000; i++){		
+		shellmemory[i].var = "none";
+		shellmemory[i].value = "none";
+	}
+}
+
+// Set key value pair
+void mem_set_value(char *var_in, char *value_in) {
+	int i;
+	for (i=0; i<1000; i++){
+		if (strcmp(shellmemory[i].var, var_in) == 0){
+			shellmemory[i].value = strdup(value_in);
+			return;
+		} 
+	}
+
+	//Value does not exist, need to find a free spot.
+	for (i=0; i<1000; i++){
+		if (strcmp(shellmemory[i].var, "none") == 0){
+			shellmemory[i].var = strdup(var_in);
+			shellmemory[i].value = strdup(value_in);
+			return;
+		} 
+	}
+
+	return;
+
+}
+
+//get value based on input key
+char *mem_get_value(char *var_in) {
+	int i;
+	for (i=0; i<1000; i++){
+		if (strcmp(shellmemory[i].var, var_in) == 0){
+			return strdup(shellmemory[i].value);
+		} 
+	}
+	return NULL;
+
+}
+
+
+void printShellMemory(){
+	int count_empty = 0;
+	for (int i = 0; i < SHELL_MEM_LENGTH; i++){
+		if(strcmp(shellmemory[i].var,"none") == 0){
+			count_empty++;
+		}
+		else{
+			printf("\nline %d: key: %s\t\tvalue: %s\n", i, shellmemory[i].var, shellmemory[i].value);
+		}
+    }
+	printf("\n\t%d lines in total, %d lines in use, %d lines free\n\n", SHELL_MEM_LENGTH, SHELL_MEM_LENGTH-count_empty, count_empty);
+}
+
+
+/*
+ * Function:  addFileToMem 
+ * 	Added in A2
+ * --------------------
+ * Load the source code of the file fp into the shell memory:
+ * 		Loading format - var stores fileID, value stores a line
+ *		Note that the first 100 lines are for set command, the rests are for run and exec command
+ *
+ *  pStart: This function will store the first line of the loaded file 
+ * 			in shell memory in here
+ *	pEnd: This function will store the last line of the loaded file 
+ 			in shell memory in here
+ *  fileID: Input that need to provide when calling the function, 
+ 			stores the ID of the file
+ * 
+ * returns: error code, 21: no space left
+ */
+
+//1.4 
+void copy_script_to_backing_store(const char* originalFilePath) {
+    char uniqueFileName[256];
+    time_t currentTime = time(NULL); // Get the current time to use as a unique identifier
+
+    // Construct the unique filename using the original filename and the current time
+    // Assuming originalFilePath points to just the filename and not a full path
+    snprintf(uniqueFileName, sizeof(uniqueFileName), "./backing_store/%s_%ld", originalFilePath, currentTime);
+
+    // Open the original file for reading
+    FILE* originalFile = fopen(originalFilePath, "r");
+    if (!originalFile) {
+        perror("Failed to open original script file");
+        return;
+    }
+
+    // Open the new file in the backing_store for writing
+    FILE* newFile = fopen(uniqueFileName, "w");
+    if (!newFile) {
+        perror("Failed to create file in backing_store");
+        fclose(originalFile); // Make sure to close the original file before returning
+        return;
+    }
+
+    // Copy the content from the original file to the new file
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), originalFile) != NULL) {
+        fputs(buffer, newFile);
+    }
+
+    // Close both files
+    fclose(originalFile);
+    fclose(newFile);
+}
+
+//1.4
+int find_first_available_frame() {
+    for (int i = 0; i < FRAME_STORE_SIZE * FRAME_SIZE; i += PAGE_LINES) {
+        if (strcmp(shellmemory[i].var, "none") == 0) {
+            return i; // Return the index of the first line of the available frame
+        }
+    }
+    return -1; // Indicate that no frame is available
+}
+
+int load_file(FILE* fp, int* pStart, int* pEnd, char* filename)
+{
+	char *line;
+    size_t i;
+    int error_code = 0;
+	bool hasSpaceLeft = false;
+	bool flag = true;
+	i=101;
+	size_t candidate;
+	while(flag){
+		flag = false;
+		for (i; i < SHELL_MEM_LENGTH; i++){
+			if(strcmp(shellmemory[i].var,"none") == 0){
+				*pStart = (int)i;
+				hasSpaceLeft = true;
+				break;
+			}
+		}
+		candidate = i;
+		for(i; i < SHELL_MEM_LENGTH; i++){
+			if(strcmp(shellmemory[i].var,"none") != 0){
+				flag = true;
+				break;
+			}
+		}
+	}
+	i = candidate;
+	//shell memory is full
+	if(hasSpaceLeft == 0){
+		error_code = 21;
+		return error_code;
+	}
+    
+    for (size_t j = i; j < SHELL_MEM_LENGTH; j++){
+        if(feof(fp))
+        {
+            *pEnd = (int)j-1;
+            break;
+        }else{
+			line = calloc(1, SHELL_MEM_LENGTH);
+			if (fgets(line, SHELL_MEM_LENGTH, fp) == NULL)
+			{
+				continue;
+			}
+			shellmemory[j].var = strdup(filename);
+            shellmemory[j].value = strndup(line, strlen(line));
+			free(line);
+        }
+    }
+
+	//no space left to load the entire file into shell memory
+	if(!feof(fp)){
+		error_code = 21;
+		//clean up the file in memory
+		for(int j = 1; i <= SHELL_MEM_LENGTH; i ++){
+			shellmemory[j].var = "none";
+			shellmemory[j].value = "none";
+    	}
+		return error_code;
+	}
+	//printShellMemory();
+    return error_code;
+}
+
+
+
+char * mem_get_value_at_line(int index){
+	if(index<0 || index > SHELL_MEM_LENGTH) return NULL; 
+	return shellmemory[index].value;
+}
+
+void mem_free_lines_between(int start, int end){
+	for (int i=start; i<=end && i<SHELL_MEM_LENGTH; i++){
+		if(shellmemory[i].var != NULL){
+			free(shellmemory[i].var);
+		}	
+		if(shellmemory[i].value != NULL){
+			free(shellmemory[i].value);
+		}	
+		shellmemory[i].var = "none";
+		shellmemory[i].value = "none";
+	}
+}
